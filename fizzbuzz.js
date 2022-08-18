290fb58897346c765ac284b1f98eef4a396d7f1c
Alexis Mayoral <99233595+alexis0501@users.noreply.github.com>
2022-08-18 16:44:16 -0700

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,18 +3,6 @@
  * @return {string[]}
  */
 
-// for (let i = 0; i < 80; i++) {
-//     if(i % 15 == 0)
-//         console.log("Fizzbuzz");
-//     else if (i % 5 == 0) 
-//         console.log("Buzz");
-//     else if (i % 3 == 0)
-//         console.log("fizz");
-//     else 
-//         console.log(i);
-// }
-
-
 for (let i = 0; i < 55; i++) {
     if(i % 15 == 0)
         console.log("fizzbuzz");
